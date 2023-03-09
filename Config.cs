aa4a2e238f244fe061c019bd1679023c16e8b18d
Brian Gembara <brian@gembara.net>
2023-03-09 00:57:32 -0500

    uncomment tab 3

--- Config.cs
+++ Config.cs
@@ -38,7 +38,7 @@ public class Cfg: OptionInterface {
 		Tabs = new[] {
 			new Menu.Remix.MixedUI.OpTab(this, "Main"),
 			new Menu.Remix.MixedUI.OpTab(this, "Names"),
-			//new Menu.Remix.MixedUI.OpTab(this, "Inspect"),
+			new Menu.Remix.MixedUI.OpTab(this, "Inspect"),
 		};
 		#region Tabs[0]
 			Tabs[0].AddItems(new Menu.Remix.MixedUI.UIelement[] {
@@ -162,63 +162,63 @@ public class Cfg: OptionInterface {
 				namelist, errs, btn_del_all, tbx_id, tbx_name, btn_add, btn_del, names_lbl, cbx_crea,
 			});
 		#endregion
-		// #region Tabs[2]
-		// 	var tbx_2_id = new Menu.Remix.MixedUI.OpTextBox(n_id, new(15f, 475f), 100f);
-		// 	var lbl_id = new Menu.Remix.MixedUI.OpLabel[6+5+1] {
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 		new(30f, 500f, ""),
-		// 	};
+		#region Tabs[2]
+			var tbx_2_id = new Menu.Remix.MixedUI.OpTextBox(n0_id, new(15f, 475f), 100f);
+			var lbl_id = new Menu.Remix.MixedUI.OpLabel[6+5+1] {
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+				new(30f, 500f, ""),
+			};
 			
-		// 	void UpdateStatLabels() {
-		// 		if(int.TryParse(tbx_2_id.value, out int id)) {
-		// 			var eid = new EntityID(-1, id);
-		// 			var personality = new AbstractCreature.Personality(eid);
+			void UpdateStatLabels() {
+				if(int.TryParse(tbx_2_id.value, out int id)) {
+					var eid = new EntityID(-1, id);
+					var personality = new AbstractCreature.Personality(eid);
 					
-		// 			lbl_id[ 0].text = $"ID: {id}";
-		// 			lbl_id[ 1].text = $"Aggression (agg): {personality.aggression}";
-		// 			lbl_id[ 2].text = $"Bravery (brv): {personality.bravery}";
-		// 			lbl_id[ 3].text = $"Dominance (dom): {personality.dominance}";
-		// 			lbl_id[ 4].text = $"Energy (nrg): {personality.energy}";
-		// 			lbl_id[ 5].text = $"Nervousness (nrv): {personality.nervous}";
-		// 			lbl_id[ 6].text = $"Sympathy (sym): {personality.sympathy}";
-		// 			lbl_id[ 7].text = $"";
-		// 			lbl_id[ 8].text = $"";
-		// 			lbl_id[ 9].text = $"";
-		// 			lbl_id[10].text = $"";
-		// 			lbl_id[11].text = $"";
-		// 		} else
-		// 			foreach(var lbl in lbl_id) lbl.text = "";
-		// 	}
+					lbl_id[ 0].text = $"ID: {id}";
+					lbl_id[ 1].text = $"Aggression (agg): {personality.aggression}";
+					lbl_id[ 2].text = $"Bravery (brv): {personality.bravery}";
+					lbl_id[ 3].text = $"Dominance (dom): {personality.dominance}";
+					lbl_id[ 4].text = $"Energy (nrg): {personality.energy}";
+					lbl_id[ 5].text = $"Nervousness (nrv): {personality.nervous}";
+					lbl_id[ 6].text = $"Sympathy (sym): {personality.sympathy}";
+					lbl_id[ 7].text = $"";
+					lbl_id[ 8].text = $"";
+					lbl_id[ 9].text = $"";
+					lbl_id[10].text = $"";
+					lbl_id[11].text = $"";
+				} else
+					foreach(var lbl in lbl_id) lbl.text = "";
+			}
 			
-		// 	tbx_2_id.OnChange += () => UpdateStatLabels();
+			tbx_2_id.OnChange += () => UpdateStatLabels();
 			
-		// 	Tabs[2].AddItems(new Menu.Remix.MixedUI.UIelement[] {
-		// 		new Menu.Remix.MixedUI.OpLabel(10f, 550f, "Inspect an ID", true),
-		// 		tbx_2_id,
-		// 		lbl_id[ 0],
-		// 		lbl_id[ 1],
-		// 		lbl_id[ 2],
-		// 		lbl_id[ 3],
-		// 		lbl_id[ 4],
-		// 		lbl_id[ 5],
-		// 		lbl_id[ 6],
-		// 		lbl_id[ 7],
-		// 		lbl_id[ 8],
-		// 		lbl_id[ 9],
-		// 		lbl_id[10],
-		// 		lbl_id[11],
-		// 	});
-		// #endregion
+			Tabs[2].AddItems(new Menu.Remix.MixedUI.UIelement[] {
+				new Menu.Remix.MixedUI.OpLabel(10f, 550f, "Inspect an ID", true),
+				tbx_2_id,
+				lbl_id[ 0],
+				lbl_id[ 1],
+				lbl_id[ 2],
+				lbl_id[ 3],
+				lbl_id[ 4],
+				lbl_id[ 5],
+				lbl_id[ 6],
+				lbl_id[ 7],
+				lbl_id[ 8],
+				lbl_id[ 9],
+				lbl_id[10],
+				lbl_id[11],
+			});
+		#endregion
 		
 		names_lbl._AddToScrollBox(namelist);
 		UpdateNamesLabel();
