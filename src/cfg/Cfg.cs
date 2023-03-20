4b0fc4743f47e708f2415bb7fe80ac73763b9c73
Brian Gembara <brian@gembara.net>
2023-03-20 04:27:08 -0400

    fix spacing

--- src/cfg/Cfg.cs
+++ src/cfg/Cfg.cs
@@ -8,17 +8,17 @@ public partial class Cfg: OptionInterface {
 			$"The version of this configuration. This value is presently used to migrate the value of the \"{nameof(Names)}\" setting across different mod versions. In the future it may be used for further attempts in forward-/backward-compatibility"
 		);
 		
-		public static Configurable<KeyCode> ToggleID    { get; } = bind(nameof(ToggleID   ), Keys.Tab, "The key that should toggle ID display on and off");
+		public static Configurable<KeyCode> ToggleID        { get; } = bind(nameof(ToggleID       ), Keys.Tab, "The key that should toggle ID display on and off");
 		public static Configurable<int>     ToggleIDMode    { get; } = bind(nameof(ToggleIDMode   ), 0,        "The mode for the ToggleID keybind. Should it be a toggle button or a \"hold\" button?");
-		public static Configurable<KeyCode> ToggleStats { get; } = bind(nameof(ToggleStats), Keys.End, "The key that should toggle personality & traits display on and off");
+		public static Configurable<KeyCode> ToggleStats     { get; } = bind(nameof(ToggleStats    ), Keys.End, "The key that should toggle personality & traits display on and off");
 		public static Configurable<int>     ToggleStatsMode { get; } = bind(nameof(ToggleStatsMode), 0,        "The mode for the ToggleStats keybind. Should it be a toggle button or a \"hold\" button?");
-		public static Configurable<bool>    ShowIDs     { get; } = bind(nameof(ShowIDs    ), false,    "Should ID labels be on at the start of the game?");
-		public static Configurable<bool>    Attrs       { get; } = bind(nameof(Attrs      ), false,    "Should the personality & skills readout be on at the start of the game?");
-		public static Configurable<bool>    Players     { get; } = bind(nameof(Players    ), true,     "Should we show ID labels for players?");
-		public static Configurable<bool>    PlyrAttr    { get; } = bind(nameof(PlyrAttr   ), false,    "Should we show personality traits for players?");
-		public static Configurable<bool>    Dead        { get; } = bind(nameof(Dead       ), false,    "Should labels disappear when the attached creature dies?");
-		public static Configurable<bool>    Objects     { get; } = bind(nameof(Objects    ), false,    "Should we show ID labels for objects?");
-		public static Configurable<bool>    Spoilers    { get; } = bind(nameof(Spoilers   ), false,    "Show potential spoilers?");
+		public static Configurable<bool>    ShowIDs         { get; } = bind(nameof(ShowIDs        ), false,    "Should ID labels be on at the start of the game?");
+		public static Configurable<bool>    Attrs           { get; } = bind(nameof(Attrs          ), false,    "Should the personality & skills readout be on at the start of the game?");
+		public static Configurable<bool>    Players         { get; } = bind(nameof(Players        ), true,     "Should we show ID labels for players?");
+		public static Configurable<bool>    PlyrAttr        { get; } = bind(nameof(PlyrAttr       ), false,    "Should we show personality traits for players?");
+		public static Configurable<bool>    Dead            { get; } = bind(nameof(Dead           ), false,    "Should labels disappear when the attached creature dies?");
+		public static Configurable<bool>    Objects         { get; } = bind(nameof(Objects        ), false,    "Should we show ID labels for objects?");
+		public static Configurable<bool>    Spoilers        { get; } = bind(nameof(Spoilers       ), false,    "Show potential spoilers?");
 		
 		public static Configurable<string> Names { get; } = bind(nameof(Names), "");
 	
