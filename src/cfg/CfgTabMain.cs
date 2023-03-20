1f10066d97626a19bcc0ec29dc7d067f5b540376
Brian Gembara <brian@gembara.net>
2023-03-20 04:29:08 -0400

    move these up now that there's no keybinds above them

--- src/cfg/CfgTabMain.cs
+++ src/cfg/CfgTabMain.cs
@@ -4,12 +4,12 @@ using Menu.Remix.MixedUI;
 public class CfgTabMain: CfgTab {
 	public CfgTabMain(): base("Main") {
 		this._AddItem(new OpLabel(10f, 550f, "General Options", true));
-		this.AddLabeledCheckbox(Cfg.ShowIDs,  new(20f, 390f));
-		this.AddLabeledCheckbox(Cfg.Attrs,    new(20f, 350f));
-		this.AddLabeledCheckbox(Cfg.Players,  new(20f, 310f));
-		this.AddLabeledCheckbox(Cfg.PlyrAttr, new(20f, 270f));
-		this.AddLabeledCheckbox(Cfg.Dead,     new(20f, 230f));
-		this.AddLabeledCheckbox(Cfg.Objects,  new(20f, 190f));
-		this.AddLabeledCheckbox(Cfg.Spoilers, new(20f, 150f));
+		this.AddLabeledCheckbox(Cfg.ShowIDs,  new(20f, 490f));
+		this.AddLabeledCheckbox(Cfg.Attrs,    new(20f, 450f));
+		this.AddLabeledCheckbox(Cfg.Players,  new(20f, 410f));
+		this.AddLabeledCheckbox(Cfg.PlyrAttr, new(20f, 370f));
+		this.AddLabeledCheckbox(Cfg.Dead,     new(20f, 330f));
+		this.AddLabeledCheckbox(Cfg.Objects,  new(20f, 290f));
+		this.AddLabeledCheckbox(Cfg.Spoilers, new(20f, 250f));
 	}
 }
