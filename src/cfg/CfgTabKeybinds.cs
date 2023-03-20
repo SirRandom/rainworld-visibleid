786ac791889bd653f34aab53b8e744f1a2dd830f
Brian Gembara <brian@gembara.net>
2023-03-20 04:28:42 -0400

    I can't believe it *just* works

--- src/cfg/CfgTabKeybinds.cs
+++ src/cfg/CfgTabKeybinds.cs
@@ -2,14 +2,35 @@ namespace fish.rainworld.visibleid;
 using Menu.Remix.MixedUI;
 
 public class CfgTabKeybinds: CfgTab {
-	public CfgTabKeybinds(): base("Keybinds")
-		=> AddItems(
+	public CfgTabKeybinds(): base("Keybinds") {
+		AddItems(
 			new OpLabel(10f, 550f, "Keybindings", true),
-			new OpLabel(20f, 500f, "Toggle ID Display", false),
-			new OpKeyBinder(Cfg.ToggleID, new(250f, 492f), new(100f, 35f))
+			new OpLabel(350f, 530f, "Toggle"),
+			new OpLabel(430f, 530f, "While Held"),
+			new OpLabel(20f, 500f, "Show ID Labels", false),
+			new OpKeyBinder(Cfg.ToggleID, new(210f, 492f), new(100f, 35f))
 				{ description = "This button will toggle overhead ID & name labels" },
-			new OpLabel(20f, 450f, "Toggle Personality & Traits Display", false),
-			new OpKeyBinder(Cfg.ToggleStats, new(250f, 442f), new(100f, 35f))
-				{ description = "This button will toggle the overhead personality & skills display" }
+			new OpRadioButtonGroupEx(Cfg.ToggleIDMode, new RadioPair(this, new(350f, 500f))),
+			new OpLabel(20f, 450f, "Show Personality & Traits", false),
+			new OpKeyBinder(Cfg.ToggleStats, new(210f, 442f), new(100f, 35f))
+				{ description = "This button will toggle the overhead personality & skills display" },
+			new OpRadioButtonGroupEx(Cfg.ToggleStatsMode, new RadioPair(this, new(350f, 450f)))
 		);
+	}
+}
+
+file class OpRadioButtonGroupEx: Menu.Remix.MixedUI.OpRadioButtonGroup {
+	public OpRadioButtonGroupEx(Configurable<int> configurable, params Menu.Remix.MixedUI.OpRadioButton[] buttons): base(configurable) => SetButtons(buttons);
+}
+
+file class RadioPair {
+	OpRadioButton[] btns = new OpRadioButton[2];
+	
+	public RadioPair(CfgTabKeybinds parent, Vec2 pos)
+		=> parent.AddItems(
+			btns[0] = new OpRadioButton(pos),
+			btns[1] = new OpRadioButton(pos.x+80f, pos.y)
+		);
+	
+	public static implicit operator OpRadioButton[] (RadioPair o) => o.btns;
 }
